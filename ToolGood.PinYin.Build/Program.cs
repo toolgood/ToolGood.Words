bcd1217d24796a1ab50cacdcf7f435973e1522a6
toolgood <toolgood@qq.com>
2016-12-20 22:02:27 +0800

    1.1.1版本, 增强拼音搜索.

--- ToolGood.PinYin.Build/Program.cs
+++ ToolGood.PinYin.Build/Program.cs
@@ -15,24 +15,57 @@ namespace ToolGood.PinYin.Build
             int index = 0x4e00;
             while (index <= 0x9fff) {
                 var ch = (char)index;
+
                 HashSet<int> ls = new HashSet<int>();
                 try {
-                    var chinese = new ChineseChar(ch);
                     var gpy = PinYinConverter.Get(ch.ToString());
                     if (gpy != ch.ToString()) {
-                        ls.Add(GetPyName(gpy));
+                        var py2 = GetPyName(gpy);
+                        if (py2 > 0) {
+                            ls.Add(py2);
+                        }
                     }
+                } catch (Exception) { }
 
-
+                try {
+                    var chinese = new ChineseChar(ch);
                     for (int i = 0; i < chinese.PinyinCount; i++) {
-                        var py = chinese.Pinyins[i].Replace("YAI","YA");
+                        var py = chinese.Pinyins[i].Replace("YAI", "YA");
                         var py2 = GetPyName(py);
-                        if (py2==-1) {
+                        if (py2 == -1) {
 
                         }
                         ls.Add(py2);
                     }
+                } catch (Exception) {
+
+                }
+                try {
+                    char c;
+                    Dict.TraditionalToSimplified(ch, out c);
+                    if (ch != c) {
+                        var chinese = new ChineseChar(c);
+                        for (int i = 0; i < chinese.PinyinCount; i++) {
+                            var py = chinese.Pinyins[i].Replace("YAI", "YA");
+                            var py2 = GetPyName(py);
+                            if (py2 == -1) {
+
+                            }
+                            ls.Add(py2);
+                        }
+                    }
                 } catch (Exception) { }
+
+
+                var py3 = GetPyName2(ch.ToString());
+                if (py3 > 0) {
+                    ls.Add(py3);
+                }
+                if (ch == '刓') {
+                    ls.Add(GetPyName("Liang"));
+                }
+
+
                 t.Add(ls);
                 index++;
             }
@@ -43,7 +76,7 @@ namespace ToolGood.PinYin.Build
             for (int i = 0; i < t.Count; i++) {
                 var item = t[i];
                 //node.Append(",");
-                if (item.Count==0) {
+                if (item.Count == 0) {
                     node.Add(-1);
                 } else {
                     node.Add((short)index);
@@ -57,18 +90,45 @@ namespace ToolGood.PinYin.Build
             index++;
             node.Add((short)index);
             for (int i = node.Count - 1; i >= 0; i--) {
-                if (node[i]==-1) {
+                if (node[i] == -1) {
                     node[i] = node[i + 1];
                 }
 
             }
 
 
-            File.WriteAllText("1.txt", string.Join(",",node));
+            File.WriteAllText("1.txt", string.Join(",", node));
             sb.Remove(0, 1);
             File.WriteAllText("2.txt", sb.ToString());
 
         }
+        //private 
+        private static Dictionary<string, string> _dict;
+
+        private static Dictionary<string, string> getDict()
+        {
+            if (_dict == null) {
+                var dict2 = "诘|Ji|揲|Ye|棓|Bei|足|Ju|栟|Ben|咯|Luo|迹|Gui|欻|Chua|耨|Nou|埏|Yan|囋|Can|噭|Chi|案|Wan|燝|Zhu|膻|Dan|汝|Zhuang|艹|Ao|磹|Tan|厖|Pang|观|Guang|窾|Kua|搂|Sou|继|Xu|房|Pang|黮|Shen|愬|Shuo|矜|Guan|盻|Pan|射|Ye|景|Ying|潠|Xun|蓧|Di|黈|Tou|从|Zong|洞|Tong|譳|Rou|鸊|Pi|桁|Hang|槱|Chao|被|Pi|擘|Bai|岂|Kai|铦|Kuo|瑱|Zhen|囝|Nan|嬛|Huan|乐|Lao|崚|Leng|蹻|Jue|浰|Li|摵|Se|梴|Yan|嶰|Jie|谌|Shen|撍|Qian|穞|Lu|黾|Meng|隩|Ao|刓|Liang|墄|Qi|擿|Zhe|能|Nan|居|Ji|及|Xi|揭|Qi|吾|Yu|扐|Cai|刓|Shu|啜|Shu|晻|Yan|兼|Xian|忒|Tei|痁|Dian|莫|Mu|宕|Tan|摘|Ti|灒|Cuan|什|Za|适|Di|逤|Suo|螫|Zhe|伈|Xin|扢|Jie|花|Hu|么|Mo|餧|Si|箐|Jing|禜|Ying|庳|Bei|硾|Chui|燋|Zhuo|棽|Shen|濊|Hun|泽|Shi|漱|Shou|摄|Nie|耆|Shi";
+                var sp = dict2.Split('|');
+                _dict = new Dictionary<string, string>();
+                for (int i = 0; i < sp.Length; i += 2) {
+                    _dict[sp[i]] = sp[i + 1];
+                }
+            }
+            return _dict;
+        }
+
+        private static int GetPyName2(string key)
+        {
+            var dict = getDict();
+            string py;
+            if (dict.TryGetValue(key, out py)) {
+                return pyName.IndexOf(py);
+            }
+            return -1;
+        }
+
+
         private static int GetPyName(string name)
         {
             name = name.Replace("0", "").Replace("1", "").Replace("2", "").Replace("3", "").Replace("4", "")
@@ -120,7 +180,8 @@ namespace ToolGood.PinYin.Build
              "Zen", "Zeng", "Zha", "Zhai", "Zhan", "Zhang", "Zhao", "Zhe", "Zhen",
              "Zheng", "Zhi", "Zhong", "Zhou", "Zhu", "Zhua", "Zhuai", "Zhuan",
              "Zhuang", "Zhui", "Zhun", "Zhuo", "Zi", "Zong", "Zou", "Zu", "Zuan",
-             "Zui", "Zun", "Zuo","Pou","Dia","Cen","Dei","Ca","Nve","Lve","Shei","Zhei"
+             "Zui", "Zun", "Zuo","Pou","Dia","Cen","Dei","Ca","Nve","Lve","Shei","Zhei",
+             "Ei","Chua","Nou","Tei"
        };
     }
 }
---
+++ ToolGood.PinYin.WordsBuild/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ToolGood.Words;
+using System.IO;
+
+namespace ToolGood.PinYin.WordsBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tt();
+
+        }
+
+        static void tt2()
+        {
+            var fs = File.OpenRead("1.txt");
+            StreamReader sr = new StreamReader(fs);
+            HashSet<string> list = new HashSet<string>();
+
+            do {
+                var text = sr.ReadLine();
+                var sp = text.Split(' ');
+                for (int i = 0; i < sp[0].Length; i++) {
+                    var c = sp[0][i];
+                    var all = WordHelper.GetAllPinYin(c);
+                    var pys = sp[1].Split('|');
+                    var py = pys[i].ToUpper();
+                    if (py.Length > 0) {
+                        py = py[0] + pys[i].Substring(1);
+                    }
+
+                    if (all.Count == 0 || all.Contains(py) == false) {
+                        list.Add(c + "|" + py);
+                    }
+                }
+            } while (sr.EndOfStream == false);
+
+            File.WriteAllText("2.txt", string.Join("|", list));
+
+        }
+
+        static void tt()
+        {
+            var fs = File.OpenRead("words.txt");
+            StreamReader sr = new StreamReader(fs);
+            StringBuilder sb = new StringBuilder();
+
+            do {
+                var text = sr.ReadLine();
+                var sp = text.Split(' ');
+                var py = WordHelper.GetPinYin(sp[0]).ToLower();
+                var py2 = sp[1].Replace("|", "");
+                if (py != py2) {
+                    sb.AppendFormat("{0} {1}\r\n", sp[0], sp[1]);
+                }
+            } while (sr.EndOfStream == false);
+
+            File.WriteAllText("1.txt", sb.ToString());
+        }
+
+    }
+}
