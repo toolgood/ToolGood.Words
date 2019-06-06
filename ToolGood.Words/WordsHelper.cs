eb681a2f8fc22400ec7ddadb5ed5775c98601933
toolgood <toolgood@qq.com>
2019-06-06 16:25:41 +0800

    修改 ToChineseRMB

--- ToolGood.Words/WordsHelper.cs
+++ ToolGood.Words/WordsHelper.cs
@@ -260,6 +260,7 @@ namespace ToolGood.Words
         /// <returns></returns>
         public static string ToChineseRMB(double x)
         {
+            if (x == 0.0) { return "零元"; }
             string s = x.ToString("#L#E#D#C#K#E#D#C#J#E#D#C#I#E#D#C#H#E#D#C#G#E#D#C#F#E#D#C#.0B0A");
             string d = Regex.Replace(s, @"((?<=-|^)[^1-9]*)|((?'z'0)[0A-E]*((?=[1-9])|(?'-z'(?=[F-L\.]|$))))|((?'b'[F-L])(?'z'0)[0A-L]*((?=[1-9])|(?'-z'(?=[\.]|$))))", "${b}${z}");
             return Regex.Replace(d, ".", m => "负元空零壹贰叁肆伍陆柒捌玖空空空空空空空分角拾佰仟萬億兆京垓秭穰"[m.Value[0] - '-'].ToString());
