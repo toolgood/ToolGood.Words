d2cfe85c0862a9b48bd66cae7c0f8324b63ad487
toolgood <toolgood@qq.com>
2020-03-27 23:01:59 +0800

    添加 简繁转换 参考文档

---
+++ csharp/ToolGood.Transformation.Build/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToolGood.Transformation.Build
+{
+    class Program
+    {
+        /// <summary>
+        /// dict 来源于  https://github.com/BYVoid/OpenCC
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
