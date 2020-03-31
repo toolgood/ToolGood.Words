fc6f871eeaf48d9107829b72a89b04de3f3c7c02
toolgood <toolgood@qq.com>
2020-03-31 23:21:22 +0800

    更新 预处理

--- csharp/ToolGood.PinYin.Pretreatment/Program.cs
+++ csharp/ToolGood.PinYin.Pretreatment/Program.cs
@@ -13,19 +13,19 @@ namespace ToolGood.PinYin.Pretreatment
         {
             // 预处理
             // 第一步 处理搜狗词库
+            Console.WriteLine("第一步 处理搜狗词库");
             if (File.Exists("scel_1.txt") == false) {
                 var scel_1 = GetWords();
                 File.WriteAllText("scel_1.txt", string.Join("\n", scel_1));
                 scel_1.Clear();
             }
-            // 第二步 精简词库  
-            if (File.Exists("scel_2.txt") == false)
-            {
+            // 第二步 精简词库              
+            Console.WriteLine("第二步 精简词库");
+            if (File.Exists("scel_2.txt") == false) {
                 var txt = File.ReadAllText("scel_1.txt");
                 var lines = txt.Split('\n');
                 Dictionary<string, string> dict = new Dictionary<string, string>();
-                foreach (var item in lines)
-                {
+                foreach (var item in lines) {
                     var sp = item.Split(' ');
                     dict[sp[0]] = sp[1];
                 }
@@ -34,6 +34,7 @@ namespace ToolGood.PinYin.Pretreatment
                 List<string> keys = dict.Select(q => q.Key).OrderBy(q => q.Length).ToList();
 
 
+                #region 删除通用的
                 keys.RemoveAll(q => q.Contains("公司") && q.Length > 5);
                 keys.RemoveAll(q => q.Contains("的") && q.Length > 5);
                 keys.RemoveAll(q => q.Contains("和") && q.Length > 7);
@@ -41,9 +42,9 @@ namespace ToolGood.PinYin.Pretreatment
                 keys.RemoveAll(q => q.Contains("或") && q.Length > 7);
                 keys.RemoveAll(q => q.Contains("与") && q.Length > 7);
                 keys.RemoveAll(q => q.Contains("用") && q.Length > 7);
+                #endregion
 
-
-
+                #region 删除结尾
 
                 keys.RemoveAll(q => q.EndsWith("厂") && q.Length > 5);
                 keys.RemoveAll(q => q.EndsWith("街道") && q.Length > 5);
@@ -80,15 +81,20 @@ namespace ToolGood.PinYin.Pretreatment
                 keys.RemoveAll(q => q.EndsWith("区") && q.Length > 7);
                 keys.RemoveAll(q => q.EndsWith("寓") && q.Length > 7);
                 keys.RemoveAll(q => q.EndsWith("装置") && q.Length > 7);
+                #endregion
 
 
                 keys.RemoveAll(q => q.StartsWith("复方") && q.Length > 5);
                 keys.RemoveAll(q => q.StartsWith("一次性") && q.Length > 5);
                 keys.RemoveAll(q => q.StartsWith("因为") && q.Length > 5);
 
-
                 keys.RemoveAll(q => q.StartsWith("中国") && q.Length > 5);
                 keys.RemoveAll(q => q.StartsWith("日本") && q.Length > 5);
+                keys.RemoveAll(q => q.StartsWith("德国") && q.Length > 5);
+                keys.RemoveAll(q => q.StartsWith("法国") && q.Length > 5);
+                keys.RemoveAll(q => q.StartsWith("英国") && q.Length > 5);
+                keys.RemoveAll(q => q.StartsWith("美国") && q.Length > 5);
+
                 #region 去省市
                 keys.RemoveAll(q => q.StartsWith("蒙古") && q.Length > 5);
                 keys.RemoveAll(q => q.StartsWith("北京") && q.Length > 5);
@@ -156,28 +162,23 @@ namespace ToolGood.PinYin.Pretreatment
                 #endregion
 
 
-                for (int i = 3; i < 6; i++)
-                {
+                for (int i = 3; i < 6; i++) {
                     var keywords = keys.Where(q => q.Length <= i).ToList();
                     WordsSearch wordsSearch = new WordsSearch();
                     wordsSearch.SetKeywords(keywords);
 
-                    for (int j = keys.Count - 1; j >= 0; j--)
-                    {
+                    for (int j = keys.Count - 1; j >= 0; j--) {
                         var key = keys[j];
                         if (key.Length <= i) { break; }
 
                         var all = wordsSearch.FindAll(key);
-                        if (all.Count > 1)
-                        {
+                        if (all.Count > 1) {
                             // Console.WriteLine(key);
                             //进行拼音测试，相同则删除
                             var py = ReplaceForPinYin(key, all, dict);
-                            if (py != null)
-                            {
+                            if (py != null) {
                                 py = py.ToLower();
-                                if (dict[key].ToLower().Trim() == py)
-                                {
+                                if (dict[key].ToLower().Trim() == py) {
                                     keys.RemoveAt(j);
                                 }
                             }
@@ -188,22 +189,19 @@ namespace ToolGood.PinYin.Pretreatment
                     GC.Collect();
                 }
 
-                for (int i = keys.Count - 1; i >= 0; i--)
-                {
+                for (int i = keys.Count - 1; i >= 0; i--) {
                     var item = keys[i];
                     if (item.Length < 5) break;
                     var pyf = WordsHelper.GetPinYinFast(item).ToLower();
 
-                    if (dict[item].ToLower().Replace(",", "") == pyf)
-                    {
+                    if (dict[item].ToLower().Replace(",", "") == pyf) {
                         keys.RemoveAt(i);
                     }
                 }
 
                 var fs = File.Open("scel_2.txt", FileMode.OpenOrCreate);
                 StreamWriter sw = new StreamWriter(fs);
-                foreach (var item in keys)
-                {
+                foreach (var item in keys) {
                     sw.WriteLine($"{item} {dict[item]}");
                 }
                 sw.Close();
@@ -211,8 +209,37 @@ namespace ToolGood.PinYin.Pretreatment
             }
 
             // 第三步 获取词的所有拼音
+            Console.WriteLine("第三步 获取词的所有拼音");
+            if (File.Exists("scel_3.txt") == false) {
+                Dictionary<char, HashSet<string>> dict = new Dictionary<char, HashSet<string>>();
+                var txt = File.ReadAllText("scel_2.txt");
+                var lines = txt.Split('\n');
+                foreach (var item in lines) {
+                    if (string.IsNullOrWhiteSpace(item)) { continue; }
+                    var sp = item.Trim().Split(' ');
+                    var pys = sp[1].Split(',');
+                    for (int i = 0; i < sp[0].Length; i++) {
+                        var c = sp[0][i];
+                        HashSet<string> list;
+                        if (dict.TryGetValue(c, out list) == false) {
+                            list = new HashSet<string>();
+                            dict[c] = list;
+                        }
+                        list.Add(pys[i]);
+                    }
+                }
+
+                var fs = File.Open("scel_3.txt", FileMode.OpenOrCreate);
+                StreamWriter sw = new StreamWriter(fs);
+                foreach (var item in dict) {
+                    sw.WriteLine($"{item.Key} {string.Join(",", item.Value)}");
+                }
+                sw.Close();
+                fs.Close();
+            }
 
             // 第四步 获取网上的拼音
+            Console.WriteLine("第四步 获取网上的拼音");
             if (File.Exists("pinyin_1.txt") == false) {
                 var pinyin_1 = GetPinYin();
                 File.WriteAllText("pinyin_1.txt", string.Join("\n", pinyin_1));
@@ -220,6 +247,7 @@ namespace ToolGood.PinYin.Pretreatment
             }
 
             // 第五步 分离 单字拼音 和 词组拼音
+            Console.WriteLine("第五步 分离 单字拼音 和 词组拼音");
             if (File.Exists("pinyin_2_one.txt") == false) {
                 var txt = File.ReadAllText("pinyin_1.txt");
                 var lines = txt.Split('\n');
@@ -238,10 +266,13 @@ namespace ToolGood.PinYin.Pretreatment
                 ones.Clear();
                 mores.Clear();
             }
+
             // 第六步 简单 合并 单字拼音， 防止常用拼音被覆盖
+            Console.WriteLine("第六步 简单 合并 单字拼音， 防止常用拼音被覆盖");
             if (File.Exists("pinyin_3_one.txt") == false) {
                 var txt = File.ReadAllText("pinyin_2_one.txt");
                 var lines = txt.Split('\n').ToList();
+                lines = lines.OrderBy(q => q).ToList();
                 for (int i = lines.Count - 1; i >= 1; i--) {
                     if (lines[i].StartsWith(lines[i - 1])) {
                         lines.RemoveAt(i);
@@ -249,7 +280,9 @@ namespace ToolGood.PinYin.Pretreatment
                 }
                 File.WriteAllText("pinyin_3_one.txt", string.Join("\n", lines));
             }
+
             // 第七步 检查 拼音数 与 词组长度不一样的
+            Console.WriteLine("第七步 检查 拼音数 与 词组长度不一样的");
             if (File.Exists("pinyin_4_ok.txt") == false) {
                 var txt = File.ReadAllText("pinyin_2_more.txt");
                 var lines = txt.Split('\n');
@@ -267,11 +300,110 @@ namespace ToolGood.PinYin.Pretreatment
                 File.WriteAllText("pinyin_4_error.txt", string.Join("\n", errors));
             }
 
+            // 第八步 单字拼音
+            Console.WriteLine("第八步 检查 单字拼音 是否出错");
+            if (File.Exists("pinyin_5_one.txt") == false) {
+                var txt = File.ReadAllText("dict/dict-pinyin.txt");
+                var lines = txt.Split('\n').ToList();
+                Dictionary<char, List<string>> pysDict = new Dictionary<char, List<string>>();
+
+                for (int i = lines.Count - 1; i >= 0; i--) {
+                    var line = lines[i];
+                    if (string.IsNullOrWhiteSpace(line)) { continue; }
+                    var sps = line.Split(" \"': ,\r[]".ToArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
+                    var key = sps[0][0];
+                    sps.RemoveAt(0);
+                    pysDict[key] = sps;
+                }
+
+                txt = File.ReadAllText("pinyin_3_one.txt");
+                lines = txt.Split('\n').ToList();
+                for (int i = lines.Count - 1; i >= 0; i--) {
+                    var line = lines[i];
+                    if (string.IsNullOrWhiteSpace(line)) { continue; }
+                    var sps = line.Split(" \"': ,\r[]".ToArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
+                    var key = sps[0][0];
+                    if (pysDict.ContainsKey(key) == false) {
+                        sps.RemoveAt(0);
+                        pysDict[key] = sps;
+                    }
+                }
+                Dictionary<char, List<string>> nopysDict = new Dictionary<char, List<string>>();
+
+                for (int i = 0x4e00; i <= 0x9FA5; i++) {
+                    var c = (char)i;
+                    if (pysDict.ContainsKey(c) == false) {
+                        var pys = WordsHelper.GetAllPinYin(c);
+                        pysDict[c] = pys;
+                        nopysDict[c] = pys;
+                    }
+                }
+
+                for (int i = 0x3400; i <= 0x4DB5; i++) {
+                    var c = (char)i;
+                    if (pysDict.ContainsKey(c) == false) {
+                        var pys = WordsHelper.GetAllPinYin(c);
+                        pysDict[c] = pys;
+                        nopysDict[c] = pys;
+                    }
+                }
+ 
+                List<string> ls = new List<string>();
+                foreach (var item in pysDict) {
+                    ls.Add($"{item.Key} {string.Join(",", item.Value)}");
+                }
+                ls = ls.OrderBy(q => q).ToList();
+
+                List<string> ls2 = new List<string>();
+                foreach (var item in nopysDict) {
+                    ls2.Add($"{item.Key} {string.Join(",", item.Value)}");
+                }
+                ls2 = ls2.OrderBy(q => q).ToList();
+
+                File.WriteAllText("pinyin_5_one.txt", string.Join("\n", ls));
+                File.WriteAllText("pinyin_5_oneNoPy.txt", string.Join("\n", ls2));
+            }
+
+            // 第九步 获取拼音集
+            Console.WriteLine("第九步 获取拼音集 ");
+            if (File.Exists("pinyin_6_pys.txt") == false) {
+                var txt = File.ReadAllText("pinyin_5_one.txt");
+                var lines = txt.Split('\n').ToList();
+
+                Dictionary<string, string> dict = new Dictionary<string, string>();
+
+                for (int i = lines.Count - 1; i >= 0; i--) {
+                    var line = lines[i];
+                    if (string.IsNullOrWhiteSpace(line)) { continue; }
+                    var sps = line.Split(" \"': ,\r".ToArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
+                    for (int j = 1; j < sps.Count; j++) {
+                        dict[sps[j]] = RemoveTone(sps[j]);
+                    }
+                }
+                List<string> ls = new List<string>();
+                foreach (var item in dict) {
+                    ls.Add($"{item.Key} {  item.Value}");
+                }
+                ls = ls.OrderBy(q => q).ToList();
+                File.WriteAllText("pinyin_6_pys.txt", string.Join("\n", ls));
+            }
+
+
 
 
 
         }
 
+
+        static string RemoveTone(string pinyin)
+        {
+            string s = "āáǎàōóǒòēéěèīíǐìūúǔùǖǘǚǜü";
+            string t = "aaaaooooeeeeiiiiuuuuvvvvv";
+            for (int i = 0; i < s.Length; i++) {
+                pinyin = pinyin.Replace(s[i].ToString(), t[i].ToString());
+            }
+            return pinyin;
+        }
 
         static string ReplaceForPinYin(string key, List<WordsSearchResult> all, Dictionary<string, string> dict)
         {
@@ -279,8 +411,7 @@ namespace ToolGood.PinYin.Pretreatment
             for (int i = 0; i < lineNodes.Length; i++) { lineNodes[i] = new LineNode(); }
             lineNodes[lineNodes.Length - 1].IsEnd = true;
 
-            foreach (var item in all)
-            {
+            foreach (var item in all) {
                 LineText lineText = new LineText() {
                     Start = item.Start,
                     End = item.End,
@@ -294,21 +425,20 @@ namespace ToolGood.PinYin.Pretreatment
         }
         static string GetPinYin(LineNode node, string pinyin)
         {
-            if (node.IsEnd)
-            {
+            if (node.IsEnd) {
                 return string.Join(",", pinyin.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries));
             }
-            foreach (var item in node.Children)
-            {
+            foreach (var item in node.Children) {
                 var py = pinyin + "," + item.PinYin;
                 var s = GetPinYin(item.NextNode, py);
-                if (s != null)
-                {
+                if (s != null) {
                     return s;
                 }
             }
             return null;
         }
+
+
 
 
         static List<string> GetWords()
