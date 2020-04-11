30d78cc1af23e8def132964483c17c707e3b0935
toolgood <toolgood@qq.com>
2020-04-11 15:35:02 +0800

    添加 PinyinMatch

---
+++ csharp/ToolGood.Words.Test/PinyinMatchTest/PinyinMatchTest.cs
@@ -0,0 +1,37 @@
+﻿using PetaTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToolGood.Words.Test
+{
+    [TestFixture]
+    public class PinyinMatchTest
+    {
+        [Test]
+        public void test3()
+        {
+            string s = "北京|天津|河北|辽宁|吉林|黑龙江|山东|江苏|上海|浙江|安徽|福建|江西|广东|广西|海南|河南|湖南|湖北|山西|内蒙古|宁夏|青海|陕西|甘肃|新疆|四川|贵州|云南|重庆|西藏|香港|澳门|台湾";
+
+            PinyinMatch match = new PinyinMatch();
+            match.SetKeywords(s.Split('|').ToList());
+
+            var all = match.Find("BJ");
+            Assert.AreEqual("北京", all[0]);
+            Assert.AreEqual(1, all.Count);
+
+            all = match.Find("北J");
+            Assert.AreEqual("北京", all[0]);
+            Assert.AreEqual(1, all.Count);
+
+            all = match.Find("S");
+            Assert.AreEqual("山东", all[0]);
+            Assert.AreEqual("江苏", all[1]);
+
+            all = match.Find("S东");
+            Assert.AreEqual("山东", all[0]);
+
+        }
+    }
+}
---
+++ csharp/ToolGood.Words/TextMatch/PinyinMatch.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using ToolGood.Words.internals;
+
+namespace ToolGood.Words
+{
+    public class PinyinMatch
+    {
+        private string[] _keywords;
+        private string[] _keywordsFirstPinyin;
+        private string[][] _keywordsPinyin;
+
+        #region SetKeywords
+        public void SetKeywords(ICollection<string> keywords)
+        {
+            _keywords = keywords.ToArray();
+            _keywordsFirstPinyin = new string[_keywords.Length];
+            _keywordsPinyin = new string[_keywords.Length][];
+            for (int i = 0; i < _keywords.Length; i++) {
+                var text = _keywords[i];
+                var pys = PinYinDict.GetPinYinList(text);
+                string fpy = "";
+                for (int j = 0; j < pys.Length; j++) {
+                    pys[j] = pys[j].ToUpper();
+                    fpy += pys[j][0];
+                }
+                _keywordsPinyin[i] = pys;
+                _keywordsFirstPinyin[i] = fpy;
+            }
+        }
+
+        public void SetKeywords(IList<string> keywords, IList<string> pinyin, char splitChar = ',')
+        {
+            _keywords = keywords.ToArray();
+            _keywordsFirstPinyin = new string[_keywords.Length];
+            _keywordsPinyin = new string[_keywords.Length][];
+            for (int i = 0; i < _keywords.Length; i++) {
+                var text = pinyin[i];
+                _keywordsPinyin[i] = text.Split(splitChar);
+                string fpy = "";
+                for (int j = 0; j < _keywordsPinyin[i].Length; j++) {
+                    fpy += _keywordsPinyin[i][0];
+                }
+                _keywordsFirstPinyin[i] = fpy;
+            }
+        }
+        #endregion
+
+
+
+        public List<string> Find(string key)
+        {
+            if (string.IsNullOrEmpty(key)) {
+                return null;
+            }
+            key = key.ToUpper();
+            var hasPinyin = Regex.IsMatch(key, "[a-zA-Z]");
+            if (hasPinyin == false) {
+                List<string> rs = new List<string>();
+                for (int i = 0; i < _keywords.Length; i++) {
+                    var keyword = _keywords[i];
+                    if (keyword.Contains(key)) {
+                        rs.Add(keyword);
+                    }
+                }
+                return rs;
+            }
+
+            var pykeys = SplitKeywords(key);
+            var minLength = int.MaxValue;
+            List<Tuple<string, string[]>> list = new List<Tuple<string, string[]>>();
+            foreach (var pykey in pykeys) {
+                var keys = pykey.Split((char)0);
+                if (minLength > keys.Length) {
+                    minLength = keys.Length;
+                }
+                MergeKeywords(keys, 0, "", list);
+            }
+
+            PinyinSearch search = new PinyinSearch();
+            search.SetKeywords(list);
+            List<String> result = new List<string>();
+            for (int i = 0; i < _keywords.Length; i++) {
+                var keywords = _keywords[i];
+                if (keywords.Length < minLength) {
+                    continue;
+                }
+                var fpy = _keywordsFirstPinyin[i];
+                var pylist = _keywordsPinyin[i];
+
+
+                if (search.Find(fpy, keywords, pylist)) {
+                    result.Add(keywords);
+                }
+            }
+            return result;
+        }
+
+
+        #region pinyinSearch
+        class PinyinSearch
+        {
+            TrieNode2[] _first;
+            string[] _keywords;
+            string[][] _keywordPinyins;
+
+            public virtual void SetKeywords(List<Tuple<string, string[]>> keywords)
+            {
+                _keywords = new string[keywords.Count];
+                _keywordPinyins = new string[keywords.Count][];
+                for (int i = 0; i < keywords.Count; i++) {
+                    _keywords[i] = keywords[i].Item1;
+                    _keywordPinyins[i] = keywords[i].Item2;
+                }
+                SetKeywords();
+            }
+
+            private void SetKeywords()
+            {
+                var root = new TrieNode();
+                Dictionary<int, List<TrieNode>> allNodeLayers = new Dictionary<int, List<TrieNode>>();
+                for (int i = 0; i < _keywords.Length; i++) {
+                    var p = _keywords[i];
+                    var nd = root;
+                    for (int j = 0; j < p.Length; j++) {
+                        nd = nd.Add((char)p[j]);
+                        if (nd.Layer == 0) {
+                            nd.Layer = j + 1;
+                            List<TrieNode> trieNodes;
+                            if (allNodeLayers.TryGetValue(nd.Layer, out trieNodes) == false) {
+                                trieNodes = new List<TrieNode>();
+                                allNodeLayers[nd.Layer] = trieNodes;
+                            }
+                            trieNodes.Add(nd);
+                        }
+                    }
+                    nd.SetResults(i);
+                }
+
+                List<TrieNode> allNode = new List<TrieNode>();
+                allNode.Add(root);
+                foreach (var trieNodes in allNodeLayers) {
+                    foreach (var nd in trieNodes.Value) {
+                        allNode.Add(nd);
+                    }
+                }
+                allNodeLayers = null;
+
+
+                for (int i = 1; i < allNode.Count; i++) {
+                    var nd = allNode[i];
+                    nd.Index = i;
+                    TrieNode r = nd.Parent.Failure;
+                    char c = nd.Char;
+                    while (r != null && !r.m_values.ContainsKey(c)) r = r.Failure;
+                    if (r == null)
+                        nd.Failure = root;
+                    else {
+                        nd.Failure = r.m_values[c];
+                        foreach (var result in nd.Failure.Results)
+                            nd.SetResults(result);
+                    }
+                }
+                root.Failure = root;
+
+
+                var allNode2 = new List<TrieNode2>();
+                for (int i = 0; i < allNode.Count; i++) {
+                    allNode2.Add(new TrieNode2());
+                }
+                for (int i = 0; i < allNode2.Count; i++) {
+                    var oldNode = allNode[i];
+                    var newNode = allNode2[i];
+
+                    foreach (var item in oldNode.m_values) {
+                        var key = item.Key;
+                        var index = item.Value.Index;
+                        newNode.Add(key, allNode2[index]);
+                    }
+                    foreach (var item in oldNode.Results) {
+                        newNode.SetResults(item);
+                    }
+                    oldNode = oldNode.Failure;
+                    while (oldNode != root) {
+                        foreach (var item in oldNode.m_values) {
+                            var key = item.Key;
+                            var index = item.Value.Index;
+                            if (newNode.HasKey(key) == false) {
+                                newNode.Add(key, allNode2[index]);
+                            }
+                        }
+                        foreach (var item in oldNode.Results) {
+                            newNode.SetResults(item);
+                        }
+                        oldNode = oldNode.Failure;
+                    }
+                }
+                allNode.Clear();
+                allNode = null;
+                root = null;
+
+                TrieNode2[] first = new TrieNode2[char.MaxValue + 1];
+                foreach (var item in allNode2[0].m_values) {
+                    first[item.Key] = item.Value;
+                }
+                _first = first;
+            }
+
+            public bool Find(string text, string hz, string[] pinyins)
+            {
+                TrieNode2 ptr = null;
+                for (int i = 0; i < text.Length; i++) {
+                    TrieNode2 tn;
+                    if (ptr == null) {
+                        tn = _first[text[i]];
+                    } else {
+                        if (ptr.TryGetValue(text[i], out tn) == false) {
+                            tn = _first[text[i]];
+                        }
+                    }
+                    if (tn != null) {
+                        if (tn.End) {
+                            foreach (var result in tn.Results) {
+                                var keyword = _keywords[result];
+                                var start = i + 1 - keyword.Length;
+                                var end = i;
+                                bool isok = true;
+                                var keywordPinyins = _keywordPinyins[result];
+
+
+                                for (int j = 0; j < keyword.Length; j++) {
+                                    var idx = start + j;
+                                    var py = keywordPinyins[j];
+                                    if (py.Length == 1 && py[0] >= 0x3400 && py[0] <= 0x9fd5) {
+                                        if (hz[idx] != py[0]) {
+                                            isok = false;
+                                            break;
+                                        }
+                                    } else {
+                                        if (pinyins[idx].StartsWith(py) == false) {
+                                            isok = false;
+                                            break;
+                                        }
+                                    }
+                                }
+                                if (isok) {
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                    ptr = tn;
+                }
+                return false;
+            }
+        }
+
+
+
+        #endregion
+
+
+        #region 合并关键字
+
+        private void MergeKeywords(string[] keys, int id, string keyword, List<Tuple<string, string[]>> list)
+        {
+            if (id >= keys.Length) {
+                list.Add(Tuple.Create(keyword, keys));
+                //list[keyword.Substring(1)] = keys;
+                //list.Add(keyword.Substring(1));
+                return;
+            }
+            var key = keys[id];
+            if (key[0] >= 0x3400 && key[0] <= 0x9fd5) {
+                var all = PinYinDict.GetAllPinYin(key[0]);
+                var fpy = new HashSet<char>();
+                foreach (var item in all) {
+                    fpy.Add(item[0]);
+                }
+                foreach (var item in fpy) {
+                    MergeKeywords(keys, id + 1, keyword + item, list);
+                }
+            } else {
+                MergeKeywords(keys, id + 1, keyword + key[0], list);
+            }
+        }
+
+        #endregion
+
+
+        #region SplitKeywords
+        /// <summary>
+        /// 初步分割
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        private List<string> SplitKeywords(string key)
+        {
+            InitPinyinSearch();
+            List<TextNode> textNodes = new List<TextNode>();
+            for (int i = 0; i <= key.Length; i++) { textNodes.Add(new TextNode()); }
+            textNodes.Last().End = true;
+            for (int i = 0; i < key.Length; i++) {
+                TextLine line = new TextLine();
+                line.Next = textNodes[i + 1];
+                line.Words = key[i].ToString();
+                textNodes[i].Children.Add(line);
+            }
+
+            var all = _wordsSearch.FindAll(key);
+            foreach (var searchResult in all) {
+                TextLine line = new TextLine();
+                line.Next = textNodes[searchResult.End + 1];
+                line.Words = searchResult.Keyword;
+                textNodes[searchResult.Start].Children.Add(line);
+            }
+
+            List<string> list = new List<string>();
+            BuildKsywords(textNodes[0], 0, "", list);
+            return list;
+        }
+        private void BuildKsywords(TextNode textNode, int id, string keywords, List<string> list)
+        {
+            if (textNode.End) {
+                list.Add(keywords.Substring(1));
+                return;
+            }
+            foreach (var item in textNode.Children) {
+                BuildKsywords(item.Next, id + 1, keywords + (char)0 + item.Words, list);
+            }
+        }
+
+        class TextNode
+        {
+            public bool End;
+            public List<TextLine> Children = new List<TextLine>();
+        }
+        class TextLine
+        {
+            public string Words;
+            public TextNode Next;
+        }
+
+        #endregion
+        #region InitPinyinSearch
+        private static WordsSearch _wordsSearch;
+        private void InitPinyinSearch()
+        {
+            if (_wordsSearch == null) {
+                HashSet<string> allPinYins = new HashSet<string>();
+                var pys = PinYinDict.PyShow;
+                for (int i = 1; i < pys.Length; i++) {
+                    allPinYins.Add(pys[i]);
+                }
+                var wordsSearch = new WordsSearch();
+                wordsSearch.SetKeywords(allPinYins.ToList());
+                _wordsSearch = wordsSearch;
+            }
+        }
+        #endregion
+    }
+}
