ec1ed08cfa0a51c4accabb247d4c92fe416c7ecd
toolgood <toolgood@qq.com>
2020-04-13 20:02:10 +0800

    修改 版本号

--- csharp/ToolGood.Words.Test/TextMatchTest/StringMatchExTest.cs
+++ csharp/ToolGood.Words.Test/TextMatchTest/StringMatchExTest.cs
@@ -88,7 +88,32 @@ namespace ToolGood.Words.Test
             var str = iwords.Replace(test, '*');
             Assert.AreEqual("我****", str);
         }
+        [Test]
+        public void test4()
+        {
+            string s = "...中国|国人|zg人";
+            string test = "我是中国人";
 
+            StringMatchEx iwords = new StringMatchEx();
+            iwords.SetKeywords(s.Split('|'));
+
+            var b = iwords.ContainsAny(test);
+            Assert.AreEqual(true, b);
+
+            var f = iwords.FindFirst(test);
+            Assert.AreEqual("国人", f);
+
+            var all = iwords.FindAll(test);
+            Assert.AreEqual("国人", all[0]);
+            Assert.AreEqual(1, all.Count);
+
+            var str = iwords.Replace(test, '*');
+            Assert.AreEqual("我是中**", str);
+
+            test = "sky是中国人";
+            f = iwords.FindFirst(test);
+            Assert.AreEqual("ky是中国", f);
+        }
 
 
 
