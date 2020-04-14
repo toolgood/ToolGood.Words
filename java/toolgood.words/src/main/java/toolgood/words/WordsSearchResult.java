a1cbbfb63417a4e249f7d6f6f065b689615338f6
toolgood <toolgood@qq.com>
2020-04-14 22:04:17 +0800

    修改 WordsSearchResult

--- java/toolgood.words/src/main/java/toolgood/words/WordsSearchResult.java
+++ java/toolgood.words/src/main/java/toolgood/words/WordsSearchResult.java
@@ -1,26 +1,32 @@
 package toolgood.words;
 
+public class WordsSearchResult {
 
-public class WordsSearchResult{
-
-    public WordsSearchResult(String keyword, int start, int end, int index)
-    {
+    public WordsSearchResult(String keyword, int start, int end, int index) {
         Keyword = keyword;
         End = end;
         Start = start;
         Index = index;
+        MatchKeyword = keyword;
     }
-    /**开始位置 */
+
+    public WordsSearchResult(String keyword, int start, int end, int index, String matchKeyword) {
+        Keyword = keyword;
+        End = end;
+        Start = start;
+        Index = index;
+        MatchKeyword = matchKeyword;
+    }
+
+    /** 开始位置 */
     public int Start;
-    /**结束位置 */
-    public int End ;
-    /**关键字 */
+    /** 结束位置 */
+    public int End;
+    /** 关键字 */
     public String Keyword;
-    /**索引 */
+    /** 索引 */
     public int Index;
+    /** 匹配关键字 */
+    public String MatchKeyword;
 
- 
-
-        
-   
 }
